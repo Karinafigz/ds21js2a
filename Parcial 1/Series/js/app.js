8ab3800ae6dcc04b55db9a35df3dd6cb7829d1c4
Karina <gfigueroa6@ucol.mx>
2021-04-06 21:04:18 -0500

    codigos

---
+++ Parcial 1/Series/js/app.js
@@ -0,0 +1,19 @@
+//1. Fibonacci
+//   fibonacci(n)
+// -> n integer
+// <- the n first fibonacci elements
+//Gabriela Karina Figueroa G.
+
+function fibonacci(numero){
+    let  k=0,m=1,n,p=1;
+    document.write(k,m);
+    for(i = 3; i<=number;i++){
+      n=k+m;
+      document.write(n);
+      p= p+n;
+      k=m;
+      m=n;
+    }
+    }
+  fibonacci(prompt());
+  
